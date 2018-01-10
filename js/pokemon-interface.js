d44bc52379bd357d9b2098cff71547b925195053
Amy Churchwell and Sailor Winkelman <sailor@>
2018-01-10 14:03:00 -0800

    Debug pokemon number return

--- js/pokemon-interface.js
+++ js/pokemon-interface.js
@@ -1,5 +1,20 @@
-// var apiKey = require('./../.env').apiKey;
+$(document).ready(function() {
+  $('#button').click(function() {
+    let search = $('#search').val();
+    $('#search').val("");
 
-// $(document).ready(function){
-//   $('#')
-// }
+    $.ajax({
+      url: `http://pokeapi.co/api/v2/pokemon/${search}/`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.solution').text(`Your result ${search} is ${response.name}`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
+});
